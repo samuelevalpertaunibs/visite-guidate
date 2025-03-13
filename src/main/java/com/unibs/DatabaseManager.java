828a2691e7298ad67ecb58cd0b3e96d1f35a28e3
a.ferrari <alberto.ferrari0329@gmail.com>
2025-03-13 12:03:24 +0100

    Fix: connection to db now use env var

--- src/main/java/com/unibs/DatabaseManager.java
+++ src/main/java/com/unibs/DatabaseManager.java
@@ -7,28 +7,28 @@ import java.sql.SQLException;
 public class DatabaseManager {
 
     private static Connection connection = null;
-    private static String url;
+    private static String ip;
     private static String port;
     private static String name;
     private static String user;
     private static String pass;
 
     public static Connection getConnection() throws SQLException {
-        if (url == null && port == null && name == null && user == null && pass == null) {
-            url = System.getenv("DB_URL");
+        if (ip == null && port == null && name == null && user == null && pass == null) {
+            ip = System.getenv("DB_URL");
             port = System.getenv("DB_PORT");
             name = System.getenv("DB_NAME");
             user = System.getenv("DB_USER");
             pass = System.getenv("DB_PASS");
 
-            if (url == null || port == null || name == null || user == null || pass == null) {
+            if (ip == null || port == null || name == null || user == null || pass == null) {
                 throw new IllegalStateException("Variabili d'ambiente per il database mancanti!\n" +
                         "Assicurati di aver impostato DB_URL, DB_PORT, DB_NAME, DB_USER e DB_PASS.");
             }
         }
 
         if (connection == null || connection.isClosed()) {
-            String url = "jdbc:mysql://localhost:3306/visite_guidate";
+            String url = String.format("jdbc:mysql://%s:%s/%s", ip, port, name);
 
             try {
                 connection = DriverManager.getConnection(url, user, pass);
