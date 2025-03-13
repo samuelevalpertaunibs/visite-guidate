8155100a787e263209b2859920bf391f866d2b5a
fejzu <d.fejzulla@studenti.unibs.it>
2025-03-13 21:06:22 +0100

    implement password hash with salt

--- src/main/java/com/unibs/LoginService.java
+++ src/main/java/com/unibs/LoginService.java
@@ -1,6 +1,9 @@
 package com.unibs;
 
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import com.unibs.models.User;
 
@@ -13,8 +16,12 @@ public class LoginService {
 
         User user = UserDao.findByUsername(username);
 
-        if (user != null && user.checkPassword(password)) {
-            return user;
+
+        if (user != null) {
+            String passwordHash = hashPassword(password, user.getSalt());
+            if (user.checkPassword(passwordHash)) {
+                return user;
+            }
         }
 
         return null;
@@ -23,16 +30,20 @@ public class LoginService {
     protected void registerUser(User user, String newPassword) throws DatabaseException, IllegalArgumentException {
         if (newPassword.isEmpty())
             throw new IllegalArgumentException("La nuova password non può essere vuota.");
-        String oldPassword = user.getPassword();
-        if (oldPassword.equals(newPassword)) {
+        String oldPassword = user.getPasswordHash();
+
+        byte [] salt = user.getSalt();
+        String hashedNewPassword = hashPassword(newPassword,salt);
+
+        if (oldPassword.equals(hashedNewPassword)) {
             throw new IllegalArgumentException("La nuova password non può essere uguale alla precedente.");
         }
-        user.setPassword(newPassword);
+        user.setPasswordHash(hashedNewPassword);
 
         try {
-            UserDao.registerUser(user.getUsername(), newPassword, LocalDate.now());
+            UserDao.registerUser(user.getUsername(), hashedNewPassword, LocalDate.now());
         } catch (DatabaseException e) {
-            user.setPassword(oldPassword);
+            user.setPasswordHash(oldPassword);
             throw e;
         }
     }
@@ -45,5 +56,24 @@ public class LoginService {
         }
 
         return null;
+    }
+    public String hashPassword(String password, byte[] salt) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        md.update(salt);
+        byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+        return bytesToHex(hashedPassword);
+    }
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder(2 * bytes.length);
+        for (byte b : bytes) {
+            hexString.append(String.format("%02x", b));
+        }
+        return hexString.toString();
     }
 }
--- src/main/java/com/unibs/models/Luogo.java
+++ src/main/java/com/unibs/models/Luogo.java
@@ -2,4 +2,5 @@ package com.unibs.models;
 
 public class Luogo {
 
+
 }
